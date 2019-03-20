6dcf3299b9fda413dd7615e7e2307047da03dfa8
An <38684206+ananJet@users.noreply.github.com>
2019-03-20 23:24:40 +0800

    Add files via upload

---
+++ 100. 相同的树/IsSameTree.java
@@ -0,0 +1,19 @@
+public class IsSameTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null)
+            return true;
+        if (! (p != null && q != null))
+            return false;
+        if (p.val != q.val)
+            return false;
+        return this.isSameTree(p.left, q.left) && this.isSameTree(p.right, q.right);
+    }
+}
